2253002bcc5c3c652c66300a9600bf7fe5d102e3
Sophie Marshall <sophie.marshall98@gmail.com>
2025-06-20 09:52:34 -0400

    Generation (#3)
    * things?
    
    * free generation lol

---
+++ utils/constants.py
@@ -0,0 +1,6 @@
+TEST_QUERIES = [
+    "tell me about yourself",
+    "what is your educational background",
+    "why are you seeking a new position",
+    "what experience do you have with data pipelines",
+]
---
+++ utils/rag.py
@@ -0,0 +1,7 @@
+class RAG:
+    def __init__(self):
+        self.postgres = None
+        self.llm = None
+
+    def _initialize():
+        self.postgres = PostgresClient
